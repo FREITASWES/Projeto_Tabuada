31f525c451d70b6d0ce351ece3a63a6162abf37b
FREITASWES <freitass.wesley@gmail.com>
2024-06-29 18:14:46 -0300

    Correção de intens, reorganizando o resultado da Tabuada

--- js/tabuada_script.js
+++ js/tabuada_script.js
@@ -1,18 +1,18 @@
-function Tabuada() {
+function Tabuada(){
     var num = window.document.getElementById('num')
     var end = document.getElementById('end')
-    if (num.value.length == 0 || end.value.length == 0) {
-        window.alert('Nenhum número digitado, [CONFIRA OS DADOS]')
+    if (num.value.length == 0 || end.value.length == 0){
+        window.alert('Nenhum número digitado, [CONFIRA OS DADOS')
     } else {
         var n = Number(num.value)
         var e = Number(end.value)
         var x = 0
         var r = ''
         var res = window.document.getElementById('resposta')
-        res.innerHTML = `A Tabuada do ${n} é: <br>`
-        do{
+        res.innerHTML = `A Tabuada do ${n} é:  <br>`
+        do {
             r = n*x
-            res.innerHTML += `${x} X ${n} = ${r} <br>`
+            res.innerHTML += `${n} x ${x} = ${r} <br>`
             x++
         } while (x <= e)
     }
